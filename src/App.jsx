87f1c52335934b542041bc4fb363f098ff9842d0
Yogesh -Figma <Yogeshui@investmango.com>
2025-04-11 14:20:28 +0530

    BMA EC2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-// Import your components
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Protected from './components/Protected';
-import Admin from './pages/Admin';
-import NewLead from './pages/NewLead';
-import OpenLeads from './pages/OpenLeads';
-import LeadsReport from './pages/LeadsReport';
-import ClosedLeads from './pages/ClosedLeads';
-import TotalLeads from './pages/TotalLeads';
-import DeclineLeads from './pages/DeclineLeads';
-import Meeting from './pages/Meeting';
-import MainOrg from './pages/MainOrg';
-import Attendance from './pages/Attendance';
-import EmployeeDashboard from './pages/EmployeeDashboard';
-import EmployeePerformance from './pages/EmployeePerformance';
-import Dashboard from './pages/Dashboard';
-import Subscription from './pages/Subscription';
-import NewCallLeads from './pages/NewCallLeads';
-import OpenCallLeads from './pages/OpenCallLeads';
-import User from './pages/User';
-import Allfib from './pages/Allfib';
-import SalaryHistory from './pages/SalaryHistory';
-import MyProfile2 from './pages/MyProfile2';
-import Payroll from './pages/Payroll';
-import Project from './pages/Project';
-import Projectli from './pages/Projectli';
+import React from "react";
+import { Provider, defaultTheme } from "@adobe/react-spectrum";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Protected from "./utils/Protected";
+import { ThemeProvider } from "./contexts/UseColoeScheme";
+import {
+  Admin,
+  ClosedLeads,
+  Dashboard,
+  DeclineLeads,
+  EmployeeDashboard,
+  LandingPage,
+  LeadsReport,
+  Login,
+  MainOrg,
+  Meeting,
+  NewLead,
+  OpenLeads,
+  TotalLeads,
+  EmployeePerformance,
+  Subscription,
+  NewCallLeads,
+  OpenCallLeads,
+  Attendance,
+  User,
+  Allfib,
+  SalaryHistory,
+  Payroll,
+  Project,
+  Projectli,
+  MyProfile2,
+  
+} from "./utils/NaviationPage";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
@@ -66,16 +73,28 @@ const router = createBrowserRouter([
       { path: "AddUser", element: <User /> },
       { path: "fibrequest", element: <Allfib /> },
       { path: "SalaryHistory", element: <SalaryHistory /> },
-      { path: "Profile", element: <MyProfile2 /> },
+      {path:'Profile',element:<MyProfile2/>},
       { path: "payroll", element: <Payroll /> },
       { path: "Projects", element: <Project /> },
-      { path: "Project", element: <Projectli /> },
+      {path:'Project',element:<Projectli/>}
+   
     ],
   },
 ]);
 
+window.addEventListener("beforeunload", () => {
+  window.history.replaceState({}, document.title, window.location.pathname);
+});
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider>
+    <Provider theme={defaultTheme}>
+      <RouterProvider router={router} />
+      <ToastContainer position="top-right" autoClose={3000} />
+    </Provider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
